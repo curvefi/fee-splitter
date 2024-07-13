c1379b4bffa6e9496a85f30db47b8851c7d3de50
Alberto <albicento.ac@gmail.com>
2024-07-13 22:14:03 +0200

    test: placeholder test for incentives manager

--- test/conftest.py
+++ test/conftest.py
@@ -2,6 +2,15 @@ import boa
 from pytest import fixture
 
 @fixture
+def crvusd():
+    return "0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E"
+
+@fixture
 def client():
     from contracts import autobribe
     return autobribe("0x9c6f7A3117c9E83b8083563f026E08E907c95e3C", "0x9c6f7A3117c9E83b8083563f026E08E907c95e3C")
+
+@fixture
+def manager(crvusd):
+    from contracts.manual import IncentivesManager
+    return IncentivesManager(crvusd)
