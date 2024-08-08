2071e088659a15035ec92c8e6663a47b3423afee
Alberto <albicento.ac@gmail.com>
2024-08-08 21:19:52 +0200

    test: increasing coverage

--- tests/unitary/incentives_manager/test_im_post_incentives.py
+++ tests/unitary/incentives_manager/test_im_post_incentives.py
@@ -27,3 +27,19 @@ def test_expected_behavior(manager_with_voting_market, generic_voting_market,
         assert payload[1] == amount
         assert payload[2] == data
 
+def test_proposer_cant_post(manager, bribe_manager, bribe_proposer):
+    payloads = [(boa.env.generate_address(), i * 10**20, bytes()) for i in range(1, 10)]
+
+    with boa.env.prank(bribe_manager):
+        for (gauge, _, _) in payloads:
+            manager.set_gauge_cap(gauge, 10**23)
+
+    with boa.env.prank(bribe_proposer):
+        manager.update_incentives_batch(payloads)
+        manager.confirm_batch()
+        with boa.reverts("manager: proposer can't post"):
+            manager.post_incentives()
+
+def test_batch_unconfirmed(manager, bribe_proposer):
+    with boa.reverts("manager: batch yet to be confirmed"):
+        manager.post_incentives()
