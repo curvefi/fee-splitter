62dda0b7c57823301861e8ff84f68969c3e8c0c2
Alberto <albicento.ac@gmail.com>
2024-08-08 16:24:48 +0200

    test: increase multiclaim ctor coverage

--- tests/unitary/fee_splitter/controller_multiclaim/test_cm_constructor.py
+++ tests/unitary/fee_splitter/controller_multiclaim/test_cm_constructor.py
@@ -4,3 +4,10 @@ import boa
 def test_expected_behavior(multiclaim_deployer):
     mc = multiclaim_deployer(factory := boa.env.generate_address())
     assert mc._immutables.factory == factory
+
+
+def test_zero_address(multiclaim_deployer):
+    zero = boa.eval("empty(address)")
+
+    with boa.reverts("zeroaddr: factory"):
+        multiclaim_deployer(zero)
