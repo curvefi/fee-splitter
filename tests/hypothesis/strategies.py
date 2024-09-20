9a952bc72e1c54c4f604cdebe62a0f2603a0828b
Alberto <albicento.ac@gmail.com>
2024-09-20 14:57:23 +0200

    test: fix search strategies

--- tests/hypothesis/strategies.py
+++ tests/hypothesis/strategies.py
@@ -1,42 +1,37 @@
 import boa
 from boa.test import strategy as boa_st
 from hypothesis import assume
-from hypothesis.strategies import booleans, composite, integers, just, lists
-
-from contracts.fee_splitter import FeeSplitter
-from tests.mocks import (
-    MockController,
-    MockControllerFactory,
-    MockDynamicWeight,
-    MockERC20,
+from hypothesis.strategies import (
+    booleans,
+    builds,
+    composite,
+    integers,
+    just,
+    lists,
 )
 
 address = boa_st("address")
 
-max_bps = 10_000
-zero = boa.eval("empty(address)")
+MAX_BPS = 10_000
+ZERO = boa.eval("empty(address)")
 
 
 @composite
 def weights(draw, n):
-    @composite
-    def sorted_unique_integers(draw):
-        # Generate n-1 unique integers between 1 and max_bps-1
-        points = draw(
+    # Generate n-1 unique integers between 1 and max_bps-1
+    points = sorted(
+        draw(
             lists(
-                integers(1, max_bps - 1),
+                integers(1, MAX_BPS - 1),
                 min_size=n - 1,
                 max_size=n - 1,
                 unique=True,
             )
         )
-        return sorted(points)
-
-    # Draw the sorted unique integers
-    points = draw(sorted_unique_integers())
+    )
 
     # Add start and end points
-    points = [0] + points + [max_bps]
+    points = [0] + points + [MAX_BPS]
 
     # Calculate differences between adjacent points to get weights
     weights = [points[i + 1] - points[i] for i in range(n)]
@@ -55,35 +50,35 @@ def receivers(draw, n=0):
     receivers_list = []
     for i in range(n):
         if is_dynamic[i]:
-            receivers_list.append((MockDynamicWeight().address, _weights[i]))
+            mock_dynamic_weight = boa.load("tests/mocks/MockDynamicWeight.vy")
+            receivers_list.append((mock_dynamic_weight.address, _weights[i]))
         else:
             receiver_address = draw(address)
-            assume(receiver_address != zero)
+            assume(receiver_address != ZERO)
             receivers_list.append((draw(address), _weights[i]))
 
     return receivers_list
 
 
-crvusd = just(MockERC20())
+crvusd = just(boa.load("tests/mocks/MockERC20.vy"))
 
 
 @composite
 def fee_splitters(draw):
     _crvusd = draw(crvusd)
 
-    _factory = MockControllerFactory()
+    _factory = boa.load("tests/mocks/MockControllerFactory.vy")
     _receivers = draw(receivers())
     _owner = draw(address)
-    assume(_owner != zero)
+    assume(_owner != ZERO)
 
-    return FeeSplitter(_crvusd, _factory, _receivers, _owner)
+    return boa.load(
+        "contracts/FeeSplitter.vy", _crvusd, _factory, _receivers, _owner
+    )
 
 
-@composite
-def controllers(draw):
-    _crvusd = draw(crvusd)
-    return MockController(_crvusd)
+controllers = builds(boa.load_partial("tests/mocks/MockController.vy"), crvusd)
 
 
 if __name__ == "__main__":
-    print(controllers().example())
+    print(weights(123).example())
